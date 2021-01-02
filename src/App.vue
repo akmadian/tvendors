0024c065379ec91ddd20eb02b82037eb5e7769e9
Ari Madian <akmadian@gmail.com>
2021-01-02 00:27:54 -0800

    Add base files

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <MainTable />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import MainTable from './components/MainTable'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MainTable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 70%;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/MainTable.vue
@@ -0,0 +1,96 @@
+<template>
+    <b-table :data=data>
+        <template v-for="column in columns">
+            <b-table-column :key="column.id" v-bind="column">
+                <template
+                    v-if="column.searchable && !column.numeric"
+                    slot="searchable"
+                    slot-scope="props">
+                    <b-input 
+                        v-model="props.filters[props.column.field]"
+                        placeholder="Search..."
+                        icon="Magnify"
+                        size="is-small"
+                    />
+                </template>
+                <template v-slot="props">
+                    {{ props.row[column.field] }}
+                </template>
+            </b-table-column>
+        </template>
+    </b-table>
+</template>
+<script>
+export default {
+    name: 'MainTable',
+    data() {
+        return {
+            data: [
+                {
+                    "name": "White2Tea",
+                    "id": "white2tea",
+                    "url": "https://white2tea.com/",
+                    "logoUrl": "",
+                    "shippingOrigin": "China",
+                    "productTypes": [
+                        "Teaware",
+                        "Tea",
+                        "Shou",
+                        "Sheng",
+                        "Black",
+                        "Oolong",
+                        "White"
+                    ]
+                },
+                {
+                    "name": "Yunnan Sourcing",
+                    "id": "yunnansourcing",
+                    "url": "https://yunnansourcing.com/",
+                    "logoUrl": "",
+                    "shippingOrigin": "China",
+                    "productTypes": [
+                        "Teaware",
+                        "Yixing",
+                        "Tea",
+                        "Shou",
+                        "Sheng",
+                        "Black",
+                        "Oolong",
+                        "White",
+                        "Green"
+                    ]
+                },
+                {
+                    "name": "Crimson Lotus",
+                    "id": "crimsonlotus",
+                    "url": "https://crimsonlotustea.com/",
+                    "logoUrl": "",
+                    "shippingOrigin": "China",
+                    "productTypes": [
+                        "Teaware",
+                        "Tea",
+                        "Shou",
+                        "Sheng",
+                    ]
+                }
+            ],
+            columns: [
+                {
+                    field: 'name',
+                    label: 'Name'
+                },
+                {
+                    field: 'shippingOrigin',
+                    label: 'Shipping Origin',
+                    searchable: true
+                },
+                {
+                    field: 'productTypes',
+                    label: 'Product Types',
+                    searchable: true
+                }
+            ]
+        }
+    }
+}
+</script>
